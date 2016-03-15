da72d5a000614f14154d276c3dc99d1138af90a8
kbroughton <kbroughton@cognitivescale.com>
2016-03-15 01:20:04 -0500

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# Validate environment variables
+
+MISSING=""
+
+[ -z "${DOMAIN}" ] && MISSING="${MISSING} DOMAIN"
+[ -z "${UPSTREAM}" ] && MISSING="${MISSING} UPSTREAM"
+[ -z "${EMAIL}" ] && MISSING="${MISSING} EMAIL"
+
+if [ "${MISSING}" != "" ]; then
+  echo "Missing required environment variables:" >&2
+  echo " ${MISSING}" >&2
+  exit 1
+fi
+
+# Default other parameters
+
+SERVER=""
+[ -n "${STAGING:-}" ] && SERVER="--server https://acme-staging.api.letsencrypt.org/directory"
+
+# Generate strong DH parameters for nginx, if they don't already exist.
+if [ ! -f /etc/ssl/dhparams.pem ]; then
+  if [ -f /cache/dhparams.pem ]; then
+    cp /cache/dhparams.pem /etc/ssl/dhparams.pem
+  else
+    openssl dhparam -out /etc/ssl/dhparams.pem 2048
+    # Cache to a volume for next time?
+    if [ -d /cache ]; then
+      cp /etc/ssl/dhparams.pem /cache/dhparams.pem
+    fi
+  fi
+fi
+
+#create temp file storage
+mkdir -p /var/cache/nginx
+chown nginx:nginx /var/cache/nginx
+
+mkdir -p /var/tmp/nginx
+chown nginx:nginx /var/tmp/nginx
+
+# Process templates
+for t in /templates/*
+do
+  dest="/etc/nginx/$(basename $t)"
+  echo "Rendering template of $dest"
+  sed -e "s/\${DOMAIN}/${DOMAIN}/g" \
+      -e "s/\${UPSTREAM}/${UPSTREAM}/" \
+      $t > $dest
+done
+
+# Initial certificate request, but skip if cached
+if [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; then
+  letsencrypt certonly \
+    --domain ${DOMAIN} \
+    --domain ${ALT_DOMAIN} \ 
+   --authenticator standalone \
+    ${SERVER} \
+    --email "${EMAIL}" --agree-tos
+fi
+
+# Template a cronjob to reissue the certificate with the webroot authenticator
+cat <<EOF >/etc/periodic/monthly/reissue
+#!/bin/sh
+
+set -euo pipefail
+
+# Certificate reissue
+letsencrypt certonly --renew-by-default \
+  --domain "${DOMAIN}" \
+  --authenticator webroot \
+  --webroot-path /etc/letsencrypt/webrootauth/ ${SERVER} \
+  --email "${EMAIL}" --agree-tos
+
+# Reload nginx configuration to pick up the reissued certificates
+#/usr/sbin/nginx -s reload
+EOF
+chmod +x /etc/periodic/monthly/reissue
+
+# Kick off cron to reissue certificates as required
+# Background the process and log to stderr
+/usr/sbin/crond -f -d 8 &
+
+echo Ready
+# Launch nginx in the foreground
+#/usr/sbin/nginx -g "daemon off;"
